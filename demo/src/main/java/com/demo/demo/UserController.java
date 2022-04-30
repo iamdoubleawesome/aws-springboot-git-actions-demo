fd7ab9cd9116e9fc30b303aa9db012a24581cd34
iamdoubleawesome <45068060+iamdoubleawesome@users.noreply.github.com>
2022-04-30 14:32:20 +0900

    Add files via upload

---
+++ demo/src/main/java/com/demo/demo/UserController.java
@@ -0,0 +1,11 @@
+package com.demo.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping
+    public String demo(){
+        return "Hello world";
+    }
+}
